bb6ebdf30c7c5ed31515e465e897a77191e3fbe8
Martin Szulecki <opensuse@sukimashita.com>
2012-02-05 00:19:44 +0100

    Fix recovery mode for older devices (0x1280/0x1281)

--- src/libirecovery.c
+++ src/libirecovery.c
@@ -371,7 +371,9 @@ irecv_error_t irecv_open(irecv_client_t* pclient) {
 
 				if (client->mode != kDfuMode) {
 					error = irecv_set_interface(client, 0, 0);
-					error = irecv_set_interface(client, 1, 1);
+					if (client->mode > kRecoveryMode2) {
+						error = irecv_set_interface(client, 1, 1);
+					}
 				} else {
 					error = irecv_set_interface(client, 0, 0);
 				}
