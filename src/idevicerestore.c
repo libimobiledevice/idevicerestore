58a4fd91026cc00182fc0aea2ab45c19f729b8d0
Martin Szulecki <m.szulecki@libimobiledevice.org>
2012-07-17 19:22:41 +0200

    main: remove file locking as it appears to hang on some platforms

--- src/idevicerestore.c
+++ src/idevicerestore.c
@@ -96,11 +96,6 @@ static int load_version_data(struct idevicerestore_client_t* client)
 	struct stat fst;
 	int cached = 0;
 
-	lock_info_t lockinfo;
-	if (lock_file(VERSION_XML ".lock", &lockinfo) != 0) {
-		error("WARNING: could not lock file\n");
-	}
-
 	if ((stat(VERSION_XML, &fst) < 0) || ((time(NULL)-86400) > fst.st_mtime)) {
 		__mkdir("cache", 0755);
 
@@ -119,10 +114,6 @@ static int load_version_data(struct idevicerestore_client_t* client)
 	char *verbuf = NULL;
 	size_t verlen = 0;
 	read_file(VERSION_XML, (void**)&verbuf, &verlen);
-
-	if (unlock_file(&lockinfo) != 0) {
-		error("WARNING: could not unlock file\n");
-	}
 
 	if (!verbuf) {
 		error("ERROR: Could not load '" VERSION_XML "'.\n");
