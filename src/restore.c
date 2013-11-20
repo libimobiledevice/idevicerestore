4e2f579c646a26d1b54ad3f461c36bfdb44195ef
Martin Szulecki <m.szulecki@libimobiledevice.org>
2013-11-20 12:49:38 +0100

    restore: Add error message for failed SEP firmware loading status

--- src/restore.c
+++ src/restore.c
@@ -603,6 +603,9 @@ int restore_handle_status_msg(restored_client_t client, plist_t msg) {
 		case 27:
 			info("Status: Failed to mount filesystems.\n");
 			break;
+		case 51:
+			info("Status: Failed to load SEP Firmware.\n");
+			break;
 		case 1015:
 			info("Status: X-Gold Baseband Update Failed. Defective Unit?\n");
 			break;
