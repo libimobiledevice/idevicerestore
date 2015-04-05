b4efc0d79c9514da370e94d040d6c10b9070e92a
Christophe Fergeau <cfergeau@redhat.com>
2015-04-05 12:20:11 +0200

    Allow to run autogen.sh outside $top_srcdir
    This makes it more convenient to do builds out of the source dir.

--- autogen.sh
+++ autogen.sh
@@ -1,15 +1,22 @@
-#!/bin/bash
-gprefix=`which glibtoolize 2>&1 >/dev/null`
-if [ $? -eq 0 ]; then 
-  glibtoolize --force
-else
-  libtoolize --force
-fi
-aclocal -I m4
-autoheader
-automake --add-missing
-autoconf
+#!/bin/sh
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+(
+    cd "$srcdir"
+    gprefix=`which glibtoolize 2>&1 >/dev/null`
+    if [ $? -eq 0 ]; then
+      glibtoolize --force
+    else
+      libtoolize --force
+    fi
+    aclocal -I m4
+    autoheader
+    automake --add-missing
+    autoconf
+)
 
 if [ -z "$NOCONFIGURE" ]; then
-    ./configure "$@"
+    "$srcdir/configure" "$@"
 fi
