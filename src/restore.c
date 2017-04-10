dff2e01afa13fe7fca8337b9f91cb03b5278ea1d
Rene Kjellerup <rk.katana.steel@gmail.com>
2017-04-10 12:18:50 -0700

    Silenced compliler errors
    and in restore.c, thread_t is not a pointer but an interger handle
    so don't initialize it from a pointer type (NULL is a pointer).

--- src/restore.c
+++ src/restore.c
@@ -2026,7 +2026,7 @@ int restore_device(struct idevicerestore_client_t* client, plist_t build_identit
 	idevice_t device = NULL;
 	restored_client_t restore = NULL;
 	restored_error_t restore_error = RESTORE_E_SUCCESS;
-	thread_t fdr_thread = NULL;
+	thread_t fdr_thread = 0;
 
 	restore_finished = 0;
 
