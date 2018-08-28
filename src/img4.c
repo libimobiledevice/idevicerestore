5b0eafc634c294f9c85c7aa756d8f2e2970484c9
Rudolf Tammekivi <rtammekivi@gmail.com>
2018-08-28 12:37:44 +0300

    restore: Add personalization tag for AppleLogo

--- src/img4.c
+++ src/img4.c
@@ -164,6 +164,8 @@ int img4_stitch_component(const char* component_name, const unsigned char* compo
 			memcpy((void*)tag, "rdtr", 4);
 		} else if (strcmp(component_name, "RestoreSEP") == 0) {
 			memcpy((void*)tag, "rsep", 4);
+		} else if (strcmp(component_name, "AppleLogo") == 0) {
+			memcpy((void*)tag, "rlgo", 4);
 		}
 	}
 
