8207daaa2ac3cb3a5107aae6aefee8ecbe39b6d4
Nikias Bassen <nikias@gmx.li>
2019-12-27 04:35:41 +0100

    Allow T2 and non-screen devices restore

--- src/recovery.c
+++ src/recovery.c
@@ -353,6 +353,10 @@ int recovery_send_applelogo(struct idevicerestore_client_t* client, plist_t buil
 	const char* component = "RestoreLogo";
 	irecv_error_t recovery_error = IRECV_E_SUCCESS;
 
+	if (!build_identity_has_component(build_identity, component)) {
+		return 0;
+	}
+
 	info("Sending %s...\n", component);
 	if (client->recovery == NULL) {
 		if (recovery_client_new(client) < 0) {
