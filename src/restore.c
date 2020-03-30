6b461dff077079a71d50c83651109eb87ed19338
Natalia <natalia@Natalias-Mac-mini.local>
2020-03-30 12:31:10 +0300

    fix warning message

--- src/restore.c
+++ src/restore.c
@@ -2307,7 +2307,7 @@ plist_t restore_get_rose_firmware_data(restored_client_t restore, struct idevice
 		component_data = NULL;
 		component_size = 0;
 	}else{
-		info("WARNING: Rap,RestoreRTKitOS node. The entry is not in the build manifest.\n");
+		info("WARNING: Could not find 'Rap,RestoreRTKitOS' node. The entry is not in the build manifest.\n");
 	}
 	ftab_write(ftab, &component_data, &component_size);
 	ftab_free(ftab);
