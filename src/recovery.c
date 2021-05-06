52f8db759040a1ee1d6ffbb694b50b646c9c41e2
Benjamin BOURGEAIS <bbourgeais@online.net>
2021-05-06 17:37:01 +0200

    recovery: Update restore boot args to match Apple Configurator 2

--- src/recovery.c
+++ src/recovery.c
@@ -149,6 +149,8 @@ int recovery_set_autoboot(struct idevicerestore_client_t* client, int enable) {
 int recovery_enter_restore(struct idevicerestore_client_t* client, plist_t build_identity) {
 	if (client->build_major >= 8) {
 		client->restore_boot_args = strdup("rd=md0 nand-enable-reformat=1 -progress");
+	} else if (client->build_major >= 20) {
+		client->restore_boot_args = strdup("rd=md0 nand-enable-reformat=1 -progress -restore");
 	}
 
 	/* upload data to make device boot restore mode */
