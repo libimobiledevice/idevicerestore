63348084d099ca5c6cc43b04cec96cb56fc78aeb
Nir Hen <nir.hen@mce-sys.com>
2025-05-05 11:07:03 +0300

    cmdLine

---
+++ idevicerestoreCmdLine/run_idevicerestore.bat
@@ -0,0 +1,36 @@
+@echo off
+REM Set the environment variables
+set MCE_USBMUXD_PORT=37326
+set DEBUG=mce*
+set USBMUXD_PID_37326=9780
+set MceRoot=C:\Program Files (x86)\MCE-100
+set mceBox=311
+set MCEHOME=C:\Program Files (x86)\MCE-100
+set TMPDIR=C:\ProgramData\MCE-100\Tmp\1fb4c2469c238c
+
+REM Display the environment variables for verification
+echo Environment variables set:
+echo MCE_USBMUXD_PORT=%MCE_USBMUXD_PORT%
+echo DEBUG=%DEBUG%
+echo USBMUXD_PID_37326=%USBMUXD_PID_37326%
+echo MceRoot=%MceRoot%
+echo mceBox=%mceBox%
+echo MCEHOME=%MCEHOME%
+echo TMPDIR=%TMPDIR%
+
+REM Run the idevicerestore command
+
+REM Start usbmuxdhost in background without waiting
+start  "c:\program files (x86)\mce-100\usbmuxdhost.exe" 37326 311 Global\MCE_OBJECT_{890ac237-b523-4617-8cd0-2bd1a33bb2b8} Global\MCE_{49facbfc-b65c-4bc5-85b0-7ac10307ec5f}_37326 "C:\Program Files (x86)\MCE-100\usbmuxdComPlugin.dll" AutoShutdownTRUE
+
+REM Brief pause to allow usbmuxd to initialize
+timeout /t 2 /nobreak >nul
+
+echo.
+echo Running idevicerestore command...
+"c:\program files (x86)\mce-100\framework\runtime\win32\x64\idevicerestore.exe" -y -e -d -P -i 5723130099220508 -C "C:\ProgramData\MCE-100\MaintenanceApps\ROMExtracts\iPhone16,1_18.4_22E240_Restore.ipsw\ext" "C:\Program Files (x86)\MCE-100\MaintenanceApps\ROM\iPhone16,1_18.4_22E240_Restore.ipsw"
+
+REM Pause to see the output before the window closes
+echo.
+echo Command execution completed.
+pause 
---
+++ idevicerestoreCmdLine/run_mce_debug.bat
@@ -0,0 +1,11 @@
+@echo off
+REM Set the DEBUG environment variable
+set DEBUG=mce*
+
+REM Run the command from cmdLineExample.txt
+REM Replace the line below with the actual command from cmdLineExample.txt
+echo Command will be executed with DEBUG=mce*
+REM Your command goes here
+
+REM Pause to see the output (optional)
+pause 
